cc7c42b70258da0d7f06c37ef5e74907c6b1620e
Jordan Vinicius <jordan.frontdev@hotmail.com>
2024-07-10 01:27:09 -0300

    implemented hotBid section and TopCollection section

--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -1,11 +1,13 @@
 import Hero from "./Hero/Hero";
 import Hotbids from "./Hotbids/Hotbids";
+import TopCollection from "./TopCollection/TopCollection";
 
 function Home() {
   return (
     <div>
       <Hero />
       <Hotbids />
+      <TopCollection />
     </div>
   );
 }
---
+++ components/home/TopCollection/TopCollection.tsx
@@ -0,0 +1,22 @@
+import { TopCollectionData } from "@/data/data";
+import TopCollectionCard from "./TopCollectionCard";
+
+function TopCollection() {
+  return (
+    <div className="pt-16 pb-12">
+      <h1 className="text-center font-bold text-3xl md:text-4xl text-blue-950">
+        Top Collection over{" "}
+        <span className="text-amber-600">Last 24 Hours</span>
+      </h1>
+      <div className="mt-16 w-[80%] mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-12">
+        {TopCollectionData.map((data) => (
+          <div key={data.id}>
+            <TopCollectionCard data={data} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default TopCollection;
